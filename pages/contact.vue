b6cfea8a448b2a3de79fe4801c055fa14738240f
waseemullahkhan <waseemullahkhan.pki@gmail.com>
2020-10-27 10:38:44 +0500

    Update contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,7 +2,7 @@
   <div id="contact-page" class="page-wrapper contact-page content-page">
     <site-hero
       title="Contact Us"
-      subtitle="Example Contact Page"
+      subtitle="Write to Us"
       image="/uploads/contact-hero.jpg"
     ></site-hero>
     <main-section theme="sidebar-right">
@@ -28,15 +28,10 @@
               <div class="content">
                 <p>
                   <strong>
-                    Edit this page in<code>/pages/contact.vue</code>to fit your
-                    needs.
+                    
                   </strong>
                 </p>
                 <p>
-                  This form doesn't actually work but would be easy enough to
-                  slap an action on to have it post wherever you'd like. Or put
-                  the netlify attribute on it and have netlify handle all your
-                  form submissions...
                 </p>
               </div>
             </article>
