d4806eb3c371d5aa8a67bc3bd33cde71bec0dbde
waseemullahkhan <waseemullahkhan.pki@gmail.com>
2020-10-27 09:47:21 +0500

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -9,8 +9,8 @@ import theCMS from '../cms/netlify/cms'
  * General Site configurations
  */
 export default {
-  siteName: 'Awake',
-  tagline: 'A Nuxt.js Theme for Netlify CMS',
+  siteName: 'Waseem Ullah Khan',
+  tagline: 'Online Digital Services',
   featureImage: '/uploads/home-hero.jpg',
   logo: 'logo-component', // 'logo-component', // or '/logo.svg' for regular image
   googleAnalytics: {
@@ -19,6 +19,10 @@ export default {
   },
   mainMenu: [
     {
+      name: 'Home',
+      link: '/'
+    },
+    {
       name: 'About',
       link: '/about'
     },
@@ -26,20 +30,6 @@ export default {
       name: 'Contact',
       link: '/contact'
     },
-    {
-      name: 'Categories',
-      link: '/categories'
-    },
-    {
-      name: 'Github Repo',
-      link: 'https://github.com/danielkellyio/awake-template',
-      target: '_blank'
-    },
-    {
-      name: 'Awake+ Premium',
-      link: 'https://awake-premium-lp.netlify.com/',
-      target: '_blank'
-    }
   ],
   hero: {
     theme: 'dark' // options: mist, light, dark
